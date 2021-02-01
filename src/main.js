a7c10e4c9756f0680b21171143fe65f86f2c7dfb
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-01 08:11:49 -0800

    Add frontend dependencies, linter, development server, clean up plugin, html plugin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Triangle from './triangle.js';
 
@@ -7,7 +10,7 @@ $(document).ready(function() {
     const length1 = $('#length1').val();
     const length2 = $('#length2').val();
     const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
+    const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
   });
